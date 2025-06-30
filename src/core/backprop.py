34375d67ebe887455ed9f9fdff29db16dfafe7e0
pushkarkh <pushkarprashant.khairnar.ext@bayer.com>
2025-06-30 10:32:02 -0500

    backprop code

--- src/core/backprop.py
+++ src/core/backprop.py
@@ -14,15 +14,24 @@ class BackProp(FeedForward):
 
     def __init__(self):
         super().__init__()
-        self.deltas = []
+        self.deltas = [np.zeros_like(s) for s in self.layers]
+        del self.deltas[0]
+        self.diff_losses_weights = [np.zeros_like(s) for s in self.weights]
+        self.diff_losses_biases = [np.zeros_like(s) for s in self.biases]
 
     def back_prop(self, label):
         self.del_final = diff_sigmoid(self.layers[-1]) * diff_binary_cross_entropy(self.layers[-1], label)
-        for i in range(len(self.layers)-2-1):
-            d_h = (self.weights[] @ self.del_final) * diff_sigmoid(self.layers[])
+        self.deltas.append(self.del_final)
+        for i in range(len(self.layers)-1, 1, -1):
+            self.deltas.append((self.weights[i-1] @ self.deltas[-1].reshape(1, self.del_final.shape[0])) * diff_sigmoid(self.layers[i-1]))
+        self.deltas = self.deltas[::-1]
 
 
     def derivatives_(self):
-        der_w = self.layers[-2] @ self.del_final
+        for i in range(len(self.deltas)):
+            der_w = self.layers[i] @ self.del_final[i].T
+            self.diff_losses_weights.append(der_w)
+            der_b = self.del_final[i].T
+            self.diff_losses_biases.append(der_b)
 
     
