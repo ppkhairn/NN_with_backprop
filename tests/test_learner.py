c9b6bfd1a66c3d541ac33231300af9d44f0e242a
pushkarDRS <pushkar.khairnar@deriskingstrategies.com>
2025-07-05 23:05:22 -0500

    remove comments

--- tests/test_learner.py
+++ tests/test_learner.py
@@ -42,40 +42,4 @@ def test_backprop(tr_data):
     tr = Learner(backprop=bp, epoch=4)
     tr.train()
 
-    
-
-    # net = NeuNet(tr_X, tr_y)
     assert 1==1
-    # net = NeuNet(tr_X, tr_y)
-    # net.input_layer()
-    # net.add_hidden_layer(3, "sigmoid")
-    # net.add_hidden_layer(2, "sigmoid")
-    # net.output_layer(1, "sigmoid")
-
-    # net.weights = []
-    # net.weights.append(np.ones((net.layers[1].shape[0], net.layers[0].shape[0]), dtype=np.float64))
-    # net.weights.append(np.ones((net.layers[2].shape[0], net.layers[1].shape[0]), dtype=np.float64))
-    # net.weights.append(np.ones((net.layers[3].shape[0], net.layers[2].shape[0]), dtype=np.float64))
-
-    # net.biases = []
-    # net.biases.append(np.array([[1, 2, 3]], dtype=np.float64).T)
-    # net.biases.append(np.array([[2, 3]], dtype=np.float64).T) #, dtype=np.float64
-    # net.biases.append(np.array([[4]], dtype=np.float64).T)
-
-    # ff = FeedForward(net)
-    # ff.forward_pass()
-
-    # bp = BackProp(ff, learning_rate, loss_function)
-    # # bp.back_prop()
-    # # weights, biases = bp.update_parameters()
-
-    # if loss_function == "binary_cross_entropy":
-    #     bp.back_prop()
-    #     weights, biases = bp.update_parameters()
-    #     assert np.allclose(weights[-1], np.array([[1.00002479, 1.00002489]]))
-    #     assert np.allclose(biases[-1], np.array([[4.00002495]]))
-    
-    # elif loss_function == "mean_squared_error":
-    #     # Expect NotImplementedError for this loss
-    #     with pytest.raises(NotImplementedError):
-    #         bp.back_prop()
